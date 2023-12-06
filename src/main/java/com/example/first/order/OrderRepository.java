8427b293a0dbd84b505152b1ba71e5d2d6a2aa0c
Sudip Neupane <sudipneupane@nchl.local>
2023-12-06 17:59:18 +0545

    refactor

--- src/main/java/com/example/first/order/OrderRepository.java
+++ src/main/java/com/example/first/order/OrderRepository.java
@@ -1,13 +1,11 @@
 package com.example.first.order;
 
-import com.example.first.cart.Cart;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
-
     @Query(value = "SELECT * FROM orders WHERE user_id = :userId ORDER BY date DESC", nativeQuery = true)
     List<Order> getOrdersByUser(long userId);
 }
